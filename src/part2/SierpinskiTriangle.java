fa051aa2d5726539d4e3f530d6225bf6277b4969
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-03-30 11:01:56 -0600

    working on the triangle

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -22,15 +22,26 @@ public class SierpinskiTriangle {
 	}
    
 	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
-		drawTriangle(a,b,c,StdDraw.YELLOW);
+		drawTriangle(a,b,c,StdDraw.GREEN);
+		if (n <= 0)
+			return;
 
+		Point2D.Double lr = new Point2D.Double((right.x + left.x) / 2.0, right.y);
+		Point2D.Double tr = new Point2D.Double((top.x + right.x) / 2.0, (top.y + right.y) / 2.0);
+		Point2D.Double tl = new Point2D.Double((top.x + left.x) / 2.0, (top.y + left.y) / 2.0);
+		drawTriangle(tl, tr, lr,  Color.GREEN );
+		sierpinski(top, tr, tl, n - 1);
+		sierpinski(tr, right, lr, n - 1);
+		sierpinski(tl, lr, left, n - 1);
     }
    
+	
 	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
 	   double[] x = {a.x, b.x, c.x};
 	   double[] y = {a.y, b.y, c.y};
 	   StdDraw.setPenColor(color);
 	   StdDraw.filledPolygon(x, y);
+	   
 	}
 	
 	public void draw(int n) {
