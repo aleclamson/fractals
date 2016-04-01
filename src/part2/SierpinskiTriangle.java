b170db21c780317ce8422ff27063efbc7ad03104
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-04-01 10:19:31 -0600

    the triangle works

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -1,28 +1,26 @@
 package part2;
+
 import java.awt.Color;
 import java.awt.geom.Point2D;
 import draw.StdDraw;
 
+public class SierpinskiTriangle {
 
-public class SierpinskiTriangle { 
-   
-	private Point2D.Double top, right, left;
 	private Color background = new Color(34, 129, 34);
-	private Color foreground = StdDraw.YELLOW;
-	
+	Point2D.Double top, right, left;
+
 	public SierpinskiTriangle() {
-		StdDraw.setXscale(0, 150); 
-		StdDraw.setYscale(75, 150); 
-		
+		StdDraw.setXscale(0, 150);
+		StdDraw.setYscale(75, 150);
+
 		top = new Point2D.Double(75, 150);
 		right = new Point2D.Double(150, 75);
 		left = new Point2D.Double(0, 75);
-		 
-		StdDraw.clear(StdDraw.GRAY); 
+
+		StdDraw.clear(StdDraw.BLACK);
 	}
-   
-	public void sierpinski(Point2D.Double a, Point2D.Double b, Point2D.Double c, int n) {
-		drawTriangle(a,b,c,StdDraw.GREEN);
+
+	public void sierpinski(Point2D.Double top, Point2D.Double right, Point2D.Double left, int n) {
 		if (n <= 0)
 			return;
 
@@ -33,25 +31,23 @@ public class SierpinskiTriangle {
 		sierpinski(top, tr, tl, n - 1);
 		sierpinski(tr, right, lr, n - 1);
 		sierpinski(tl, lr, left, n - 1);
-    }
-   
-	
+
+	}
+
 	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
-	   double[] x = {a.x, b.x, c.x};
-	   double[] y = {a.y, b.y, c.y};
-	   StdDraw.setPenColor(color);
-	   StdDraw.filledPolygon(x, y);
-	   
+		double[] x = { a.x, b.x, c.x };
+		double[] y = { a.y, b.y, c.y };
+		StdDraw.setPenColor(color);
+		StdDraw.filledPolygon(x, y);
 	}
-	
+
 	public void draw(int n) {
-		sierpinski(top, right, left, n);
+		drawTriangle(top, right, left,  Color.BLUE);
+		sierpinski(this.top, this.right, this.left, n-1);
 	}
-	
-	
-	
-	public static void main(String[] args) { 
+
+	public static void main(String[] args) {
 		SierpinskiTriangle triangle = new SierpinskiTriangle();
-		triangle.draw(3);
+		triangle.draw(8);
 	}
 }
