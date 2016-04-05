fcae79ecc6c09e46159f8225c99f518beb83dc04
Alec Lamson <lamsonak@s.dcsdk12.org>
2016-04-05 10:23:07 -0600

    worked on the chnaing of the code with an interface

--- src/part2/SierpinskiTriangle.java
+++ src/part2/SierpinskiTriangle.java
@@ -1,23 +1,30 @@
 package part2;
 
 import java.awt.Color;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.MouseEvent;
 import java.awt.geom.Point2D;
-import draw.StdDraw;
+import draw.UltraDraw;
 
-public class SierpinskiTriangle {
-
+public class SierpinskiTriangle implements MouseListener, MouseMotionListener {
+	public static int recursion = 0;
 	private Color background = new Color(34, 129, 34);
 	Point2D.Double top, right, left;
 
 	public SierpinskiTriangle() {
-		StdDraw.setXscale(0, 150);
-		StdDraw.setYscale(75, 150);
+		UltraDraw.addMouseListener(this);
+		UltraDraw.addMouseMotionListener(this);
+		
+		
+		UltraDraw.setXscale(0, 150);
+		UltraDraw.setYscale(75, 150);
 
 		top = new Point2D.Double(75, 150);
 		right = new Point2D.Double(150, 75);
 		left = new Point2D.Double(0, 75);
 
-		StdDraw.clear(StdDraw.BLACK);
+		UltraDraw.clear(UltraDraw.GRAY);
 	}
 
 	public void sierpinski(Point2D.Double top, Point2D.Double right, Point2D.Double left, int n) {
@@ -27,7 +34,7 @@ public class SierpinskiTriangle {
 		Point2D.Double lr = new Point2D.Double((right.x + left.x) / 2.0, right.y);
 		Point2D.Double tr = new Point2D.Double((top.x + right.x) / 2.0, (top.y + right.y) / 2.0);
 		Point2D.Double tl = new Point2D.Double((top.x + left.x) / 2.0, (top.y + left.y) / 2.0);
-		drawTriangle(tl, tr, lr,  Color.GREEN );
+		drawTriangle(tl, tr, lr,  Color.BLACK );
 		sierpinski(top, tr, tl, n - 1);
 		sierpinski(tr, right, lr, n - 1);
 		sierpinski(tl, lr, left, n - 1);
@@ -37,17 +44,53 @@ public class SierpinskiTriangle {
 	public void drawTriangle(Point2D.Double a, Point2D.Double b, Point2D.Double c, Color color) {
 		double[] x = { a.x, b.x, c.x };
 		double[] y = { a.y, b.y, c.y };
-		StdDraw.setPenColor(color);
-		StdDraw.filledPolygon(x, y);
+		UltraDraw.setPenColor(color);
+		UltraDraw.filledPolygon(x, y);
 	}
 
 	public void draw(int n) {
-		drawTriangle(top, right, left,  Color.BLACK);
+		drawTriangle(top, right, left,  Color.WHITE);
 		sierpinski(this.top, this.right, this.left, n-1);
 	}
 
 	public static void main(String[] args) {
 		SierpinskiTriangle triangle = new SierpinskiTriangle();
-		triangle.draw(5);
+		triangle.draw(4);
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		System.out.println(e);
+		recursion++;
+		if (recursion <= 10) {
+			
+		}
+		SierpinskiTriangle triangle = new SierpinskiTriangle();
+		triangle.draw(recursion);
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
 	}
 }
